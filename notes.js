730cbf81e8588f6583a14e506d5850cf118f41af
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2023-09-01 15:22:06 +0530

    Optimize add notes code

--- notes.js
+++ notes.js
@@ -10,14 +10,16 @@ const addNote = (title, body) => {
       title: title,
       body: body,
     });
-    //saveNotes(notes);
-    const dataJson = JSON.stringify(notes);
-    fs.writeFileSync("notes.json", dataJson);
-
+    saveNotes(notes);
     console.log(chalk.green.inverse("New note added!"));
   } else {
     console.log(chalk.red.inverse("Note title taken!"));
   }
+};
+
+const saveNotes = (notes) => {
+  const dataJson = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJson);
 };
 
 const loadNotes = () => {
