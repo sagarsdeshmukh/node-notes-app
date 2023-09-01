5f7e80d8130e18d3937972219a3b7796ae168585
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2023-09-01 19:00:57 +0530

    add read note command

--- notes.js
+++ notes.js
@@ -54,8 +54,21 @@ const listNotes = () => {
   });
 };
 
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+
+  if (note) {
+    console.log(chalk.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("Note not found!"));
+  }
+};
+
 module.exports = {
   addNote: addNote,
   removeNote: removeNote,
   listNotes: listNotes,
+  readNote: readNote,
 };
